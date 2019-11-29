4a47d14ffe15bcad0bfc4f35506a4ef43347b53a
e <guodong@sjdd.com.cn>
2019-11-29 10:16:29 +0800

    修改了webpack配置，解决打包空白问题

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  // 基本路径
+  publicPath: './',
+  // 输出文件目录
+  outputDir: 'dist',
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: true,
+  // webpack配置
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  chainWebpack: () => {},
+  configureWebpack: () => {},
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: true,
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: null, // 设置代理
+    before: app => {}
+  },
+  // 第三方插件配置
+  pluginOptions: {
+  // ...
+  }
+}
