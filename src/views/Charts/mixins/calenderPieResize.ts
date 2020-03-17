dfb7de1aca6633d62675b82a55e064395495bca1
guodong <guodong@sjdd.com.cn>
2020-03-17 10:26:59 +0800

    解决了移动端拖动页面整体上下滑动的问题

--- src/views/Charts/mixins/calenderPieResize.ts
+++ src/views/Charts/mixins/calenderPieResize.ts
@@ -14,6 +14,7 @@ declare module 'vue/types/vue' {
 
 @Component
 export default class myMixins extends Vue {
+  // 获取更新后的饼图数据并进行数据简单处理
   public getPieSeriesUpdate(scatterData, chart) {
     return echarts['util'].map(scatterData, function (item, index) {
       var center = chart.convertToPixel('calendar', item);
